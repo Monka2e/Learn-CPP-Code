35ac8222e742eabf21f8cf7d24723f9fba0abe6d
Monka2e <IDMBaldwin@gmail.com>
2024-01-01 20:14:42 -0800

    1.11 first program - now prints three times the user input also

--- 01_11_FirstProgram/FirstProgram.cpp
+++ 01_11_FirstProgram/FirstProgram.cpp
@@ -2,10 +2,12 @@
 
 int main() {
     std::cout << "Enter an integer: ";
+    
     int x{};
     std::cin >> x;
 
-    std::cout << "Double that number is: " << x * 2 << '\n';
+    std::cout << "Double " << x << " is: " << x * 2 << '\n';
+    std::cout << "Triple " << x << " is: " << x * 3 << '\n';
 
     return 0;
 }
