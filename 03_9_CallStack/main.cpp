ddff5741ab0436e11c8eff5a07cc255217f5a93d
Monka2e <IDMBaldwin@gmail.com>
2024-01-04 13:10:16 -0800

    3.9 call stack - code imported from lesson 3.9

---
+++ 03_9_CallStack/main.cpp
@@ -0,0 +1,21 @@
+// code taken from chapter 3.9 (learncpp.com)
+#include <iostream>
+
+void a()
+{
+	std::cout << "a() called\n";
+}
+
+void b()
+{
+	std::cout << "b() called\n";
+	a();
+}
+
+int main()
+{
+	a();
+	b();
+
+	return 0;
+}
