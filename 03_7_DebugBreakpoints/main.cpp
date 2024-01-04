80ce9dd190fa3af4e26431f211d844a78bd4d069
Monka2e <IDMBaldwin@gmail.com>
2024-01-04 11:43:12 -0800

    3.7 debugging - import code from lesson

---
+++ 03_7_DebugBreakpoints/main.cpp
@@ -0,0 +1,16 @@
+// This is code imported from chapter 3.7 (learncpp.com)
+#include <iostream>
+
+void printValue(int value)
+{
+    std::cout << value << '\n';
+}
+
+int main()
+{
+    printValue(5);
+    printValue(6);
+    printValue(7);
+
+    return 0;
+}
