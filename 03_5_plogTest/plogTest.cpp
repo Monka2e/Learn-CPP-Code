5fd631ec95796ba971801b842c0d777211ef7a3b
Monka2e <IDMBaldwin@gmail.com>
2024-01-03 21:49:51 -0800

    3.5 plog implement

---
+++ 03_5_plogTest/plogTest.cpp
@@ -0,0 +1,28 @@
+// plog - https://github.com/SergiusTheBest/plog
+// After this comment except line 14 is all code directly copied from chapter 3.5
+#include <plog/Log.h> // Step 1: include the logger headers
+#include <plog/Initializers/RollingFileInitializer.h>
+#include <iostream>
+
+int getUserInput()
+{
+	PLOGD << "getUserInput() called"; // PLOGD is defined by the plog library
+
+	std::cout << "Enter a number: ";
+	int x{};
+	std::cin >> x;
+    PLOGD << "user entered " << x << '\n';
+	return x;
+}
+
+int main()
+{
+	plog::init(plog::debug, "Logfile.txt"); // Step 2: initialize the logger
+
+	PLOGD << "main() called"; // Step 3: Output to the log as if you were writing to the console
+
+	int x{ getUserInput() };
+	std::cout << "You entered: " << x << '\n';
+
+	return 0;
+}
