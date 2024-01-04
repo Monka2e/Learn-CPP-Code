2c7cd978e186cdda520ad0decf51c9b0a5c9a088
Monka2e <IDMBaldwin@gmail.com>
2024-01-04 12:48:56 -0800

    3.8 watch debugging - import code from chapter 3.8

---
+++ 03_8_DebugWatchingVariables/main.cpp
@@ -0,0 +1,16 @@
+// This code is taken from lesson 3.9 (learncpp.com)
+#include <iostream>
+
+int main()
+{
+	int x{ 1 };
+	std::cout << x << ' ';
+
+	x = x + 2;
+	std::cout << x << ' ';
+
+	x = x + 3;
+	std::cout << x << ' ';
+
+	return 0;
+}
