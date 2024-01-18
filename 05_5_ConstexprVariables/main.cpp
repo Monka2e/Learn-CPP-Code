dc0aca344ae61fe17d4ee95be2c3e8e6552ebf92
Monka2e <IDMBaldwin@gmail.com>
2024-01-17 16:31:47 -0800

    5.5 Constexpr variables

---
+++ 05_5_ConstexprVariables/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int five() {
+    return 5;
+}
+
+int main() {
+    // Initialized with constant expressions
+    constexpr double gravity {9.8};
+    constexpr char c {'t'};
+    constexpr int sum {5 + 7};
+    constexpr int total {sum};
+    constexpr double num {static_cast<double>(total)};
+
+    int x{};
+    std::cin >> x;
+
+    int u {5};
+
+    // Initialized with non constant expression
+    const int y {x};
+    const int p {u};
+    const int n {five()};
+}
