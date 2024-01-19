356f8971024f70feea90ee098da12793a6a43dfc
Monka2e <IDMBaldwin@gmail.com>
2024-01-18 21:53:44 -0800

    5.9 std::string intro

--- 05_9_stdStringIntro/main.cpp
+++ 05_9_stdStringIntro/main.cpp
@@ -1,9 +1,18 @@
-#include <string>
 #include <iostream>
+#include <string>
+#include <typeinfo>
+int main()
+{
+    std::cout << "Pick 1 or 2: ";
+    int choice {};
+    std::cin >> choice;
 
-int main() {
-    std::string s {"YO"};
-    std::cout << "Your string is: \"" << s << "\"\n";
+    std::cout << "Now enter your name: ";
+    std::string name {};
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << "Hello, " << name << ", you picked " << choice << '\n';
+    std::cout << "The length of your name is " << name.length() << " characters\n";
 
     return 0;
 }
