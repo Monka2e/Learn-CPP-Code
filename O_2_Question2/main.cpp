2156474b8af555767dde045e9f0fa967bcb4a0e3
Monka2e <IDMBaldwin@gmail.com>
2024-01-21 17:23:38 -0800

    O.2 Question 2 solved

--- O_2_Question2/main.cpp
+++ O_2_Question2/main.cpp
@@ -1,14 +1,15 @@
 #include <bitset>
 #include <iostream>
 
-// "rotl" stands for "rotate left"
-std::bitset<4> rotl(std::bitset<4> bits)
-{
-// Your code here
+std::bitset<4> rotl(std::bitset<4> bits) {
+    bool lastBit {bits.test(3)};
+    bits <<= 1;
+    if (lastBit)
+        bits.set(0);
+    return bits;
 }
 
-int main()
-{
+int main() {
 	std::bitset<4> bits1{ 0b0001 };
 	std::cout << rotl(bits1) << '\n';
 
