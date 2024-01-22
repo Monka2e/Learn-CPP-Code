386782146a950e34fe8112720874a0264573984a
Monka2e <IDMBaldwin@gmail.com>
2024-01-21 18:40:49 -0800

    O.3 Question 1 part a solved

--- O_3_Question1/main.cpp
+++ O_3_Question1/main.cpp
@@ -2,8 +2,7 @@
 #include <cstdint>
 #include <iostream>
 
-int main()
-{
+int main() {
     [[maybe_unused]] constexpr std::uint8_t option_viewed{ 0x01 };
     [[maybe_unused]] constexpr std::uint8_t option_edited{ 0x02 };
     [[maybe_unused]] constexpr std::uint8_t option_favorited{ 0x04 };
@@ -12,7 +11,7 @@ int main()
 
     std::uint8_t myArticleFlags{ option_favorited };
 
-    // Place all lines of code for the following quiz here
+    myArticleFlags |= option_viewed;
 
     std::cout << std::bitset<8>{ myArticleFlags } << '\n';
 
