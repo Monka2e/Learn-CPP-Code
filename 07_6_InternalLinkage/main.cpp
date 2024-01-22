509a6765cf3e3422954ab743d8ba01f99c8afd34
Monka2e <IDMBaldwin@gmail.com>
2024-01-21 22:49:30 -0800

    7.6 update internal linkage

--- 07_6_InternalLinkage/main.cpp
+++ 07_6_InternalLinkage/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-static constexpr int g_someValue {444};
+static constexpr int g_someValue {444}; // okay to have static variable initialized here (scope is this file)
 
 // static void printSomething(); not okay, static functions and variables can only be used in the same file
 
+static void printSomething() { // okay to have same name static function defined
+    std::cout << g_someValue * 2 << '\n';
+}
+
 int main() {
-    // printSomething();
+    printSomething();
     std::cout << g_someValue << '\n';
 
     return 0;
