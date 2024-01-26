6a83a9220d330d83a055901a5bfa195c05950ae4
Monka2e <IDMBaldwin@gmail.com>
2024-01-26 10:35:39 -0800

    8.8 Question 3 solved

---
+++ 08_8_Question3/main.cpp
@@ -0,0 +1,26 @@
+/*
+Invert the nested loops example so it prints the following:
+
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+*/
+
+#include <iostream>
+
+int main() {
+    int outer {5};
+    while (outer > 0) {
+        int inner {outer};
+        while (inner > 0) {
+            std::cout << inner << ' ';
+            --inner;
+        }
+        std::cout << '\n';
+        --outer;
+    }
+
+    return 0;
+}
