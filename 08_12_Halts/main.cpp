64854565200ca4ca81e14ca3ce522bdec4d7395a
Monka2e <IDMBaldwin@gmail.com>
2024-01-26 12:33:03 -0800

    8.12 Halts

---
+++ 08_12_Halts/main.cpp
@@ -0,0 +1,30 @@
+// This is the authors code not mine
+#include <cstdlib> // for std::exit()
+#include <iostream>
+
+void cleanup()
+{
+    // code here to do any kind of cleanup required
+    std::cout << "cleanup!\n";
+}
+
+int main()
+{
+    // register cleanup() to be called automatically when std::exit() is called
+    std::atexit(cleanup); // note: we use cleanup rather than cleanup() since we're not making a function call to cleanup() right now
+
+    std::cout << 1 << '\n';
+
+    std::exit(0); // terminate and return status code 0 to operating system
+
+    // The following statements never execute
+    std::cout << 2 << '\n';
+
+    return 0;
+}
+
+/* From 8.12
+The std::abort() function causes your program to terminate abnormally.
+Abnormal termination means the program had some kind of unusual runtime error and the program couldn’t continue to run.
+For example, trying to divide by 0 will result in an abnormal termination. std::abort() does not do any cleanup.
+*/
