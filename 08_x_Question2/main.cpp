337183ecc15035ec09914fd2a77921cb705207a7
Monka2e <IDMBaldwin@gmail.com>
2024-01-29 12:24:50 -0800

    8.x Question 2 Extra credit solved

--- 08_x_Question2/main.cpp
+++ 08_x_Question2/main.cpp
@@ -1,17 +1,29 @@
 /*
 A prime number is a natural number greater than 1 that is evenly divisible (with no remainder) only by 1 and itself.
 Complete the following program by writing the isPrime() function using a for-loop. When successful, the program will print “Success!”.
+
+Extra credit:
+
+The for-loop in the above solution is suboptimal for two reasons:
+
+It checks even numbers. We know these aren’t prime (except for 2).
+It checks all numbers through x to see if they are a divisor.A non-prime number (a composite number)
+must have at least one divisor less than or equal to its square root.
+std::sqrt(x) (in the <cmath> header) returns the square root of x.
+Update the above solution to implement both of these optimizations.
 */
 
 #include <cassert>
+#include <cmath>
 #include <iostream>
 
 bool isPrime(int x)
 {
-    if (x <= 1) {
+    if (x <= 1 || (x != 2 && x % 2 == 0)) {
         return false;
     }
-    for (int i {2}; i < x; ++i) {
+    int squareRoot {static_cast<int>(std::sqrt(x))};
+    for (int i {squareRoot}; i > 2; i -= 2) {
         if (x % i == 0) {
             return false;
         }
