71d51dac1b10708881de94f591b840e3ac902420
Monka2e <IDMBaldwin@gmail.com>
2024-01-29 13:06:46 -0800

    8.x Question 3 solved

--- 08_x_Question3/main.cpp
+++ 08_x_Question3/main.cpp
@@ -48,8 +48,45 @@ Thank you for playing.
 #include "Random.h"
 #include <iostream>
 
-int main() {
+bool queryReplay() {
+    char input {};
+    while (input != 'y' && input != 'n') {
+        std::cout << "Would you like to play again (y/n)? ";
+        std::cin >> input;
+    }
+    return input == 'y';
+}
 
+void play() {
+    int answer {Random::get(1, 100)};
+    int guess {};
+
+    std::cout << "Let's play a game. I'm thinking of a number between 1 and 100. You have 7 tries to guess what it is.\n";
+
+    for (int g {1}; g <= 7; ++g) {
+        std::cout << "Guess #" << g << ": ";
+        std::cin >> guess;
+        
+        if (guess > answer) {
+            std::cout << "Your guess is too high.\n";
+        } else if (guess < answer) {
+            std::cout << "Your guess is too low.\n";
+        } else {
+            std::cout << "Correct! You win!\n";
+            return;
+        }
+    }
+
+    std::cout << "Sorry, you lose. The correct number was " << answer << ".\n";
+}
+
+int main() {
+    bool playing {true};
+    while (playing) {
+        play();
+
+        playing = queryReplay();
+    }
 
     return 0;
 }
