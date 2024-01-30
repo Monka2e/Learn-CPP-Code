dc403a62cc5c1a8d5e4c2830a31d7a57666c6f9d
Monka2e <IDMBaldwin@gmail.com>
2024-01-29 16:56:45 -0800

    9.5 std::cin input validation

--- 09_5_InputValidationWithStdCin/main.cpp
+++ 09_5_InputValidationWithStdCin/main.cpp
@@ -1,23 +1,61 @@
+#include <limits> // for std::numeric_limits
 #include <iostream>
 
-double getDouble()
-{
-    std::cout << "Enter a decimal number: ";
-    double x{};
-    std::cin >> x;
-    return x;
+void ignoreLine() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-char getOperator()
-{
-    std::cout << "Enter one of the following: +, -, *, or /: ";
-    char op{};
-    std::cin >> op;
-    return op;
+double getDouble() {
+    while (true) {
+        std::cout << "Enter a decimal number: ";
+        double x{};
+        std::cin >> x;  
+
+        if (!std::cin) { // If previous extraction failed
+            if (std::cin.eof()) {
+                exit(0);
+            }
+
+            std::cin.clear(); // change back to normal state from failed state
+            ignoreLine();
+            continue;
+        } else if (!std::cin.eof() && std::cin.peek() != '\n') { // If previous extraction succeeded but there is leftover input in the buffer
+            ignoreLine();
+            continue;
+        }
+
+        return x;
+    }
 }
 
-void printResult(double x, char operation, double y)
-{
+char getOperator() {
+    while (true) {
+        std::cout << "Enter one of the following: +, -, *, or /: ";
+        char op{};
+        std::cin >> op;
+
+        if (!std::cin) {
+            if (std::cin.eof()) {
+                exit(0);
+            }
+
+            std::cin.clear();
+        }
+        ignoreLine();
+
+        switch(op) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return op;
+        default:
+            std::cout << "Oops, that input is invalid. Please try again.\n";
+        }
+    }
+}
+
+void printResult(double x, char operation, double y) {
     switch (operation)
     {
     case '+':
@@ -35,8 +73,7 @@ void printResult(double x, char operation, double y)
     }
 }
 
-int main()
-{
+int main() {
     double x{ getDouble() };
     char operation{ getOperator() };
     double y{ getDouble() };
