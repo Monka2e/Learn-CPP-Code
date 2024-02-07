b2245cdfaa3d2a6e2c52d95976e8e3463d787d28
Monka2e <IDMBaldwin@gmail.com>
2024-02-07 13:41:13 -0800

    11.x Question 2 step 2

--- 11_x_Question2/main.cpp
+++ 11_x_Question2/main.cpp
@@ -14,6 +14,29 @@ int main()
 
 	return 0;
 }
+
+
+> Step #2
+
+Write a function template named mult() that allows the user to multiply one value of any type (first parameter)
+and an integer (second parameter). The function should return the same type as the first parameter.
+The following program should run:
+
+#include <iostream>
+
+// write your mult function template here
+
+int main()
+{
+	std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
+
+	return 0;
+}
+and produce the following output:
+
+6
+3.6
 */
 
 #include <iostream>
@@ -23,10 +46,18 @@ T add(T x, T y) {
     return x + y;
 }
 
+template <typename T>
+T mult(T x, int y) {
+    return x * y;
+}
+
 int main()
 {
 	std::cout << add(2, 3) << '\n';
 	std::cout << add(1.2, 3.4) << '\n';
+
+    std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
 
 	return 0;
 }
